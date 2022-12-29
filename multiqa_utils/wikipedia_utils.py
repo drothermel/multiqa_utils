0ca98303e3a5c9815c922d438a2d7c3274e5c98a
Danielle Rothermel <ddr8143@nyu.edu>
2022-12-29 12:46:19 -0500

    extract useful parts of nb to utils and delete unneeded nb

--- multiqa_utils/wikipedia_utils.py
+++ multiqa_utils/wikipedia_utils.py
@@ -2,6 +2,14 @@ import jsonlines
 import html
 import re
 
+# After wikiextractor has already processed the wikidump then we can
+# use this method to do more postprocessing on the dump
+# 
+# TODO: where was this pulled from?
+# This will:
+#    1) Unescape the text
+#    2) Append the keywords "Title:" and "Article:" along with the title to the text
+#    3) Format the final output file into a .jsonl in the format expected by...? (TODO)
 def load_postprocess_dump(infile, outfile):
     clean = re.compile('<.*?>')
 
@@ -19,3 +27,71 @@ def load_postprocess_dump(infile, outfile):
     with jsonlines.open(outfile, mode='w') as writer:
         writer.write_all(postprocess_pages)
     print(f">> Wrote: {outfile}")
+
+    
+# ==== Utils for extracting metadata from wikipedia segments ==== #
+    
+def get_wikiseg_path(wikipath, segment):
+    return f"{wikipath}/{segment}"
+
+
+def get_metadata_path(wikipath, segment):
+    return f"{get_wikiseg_path(wikipath, segment)}/metadata.json"
+
+
+# Read in the segment, extract all the titles that have text and store them in a
+# metadata.json file in the same segment directory.
+# 
+# Example:
+# wikipath = '/scratch/ddr8143/wikipedia/enwiki_20220701/'
+# for i, segment in enumerate(sorted(os.listdir(wikipath))):
+#    get_segment_metadata(wikipath, segment, force=False)
+# 
+# >> Metadata exists: /scratch/ddr8143/wikipedia/enwiki_20220701/AA/metadata.json
+# ...
+#
+# json.load(open('/scratch/ddr8143/wikipedia/enwiki_20220701/AE/metadata.json')).keys()
+# >> dict_keys(['all_titles', 'titles_with_text'])
+def get_segment_metadata(wikipath, segment, force=False):
+    mdpath = get_metadata_path(wikipath, segment)
+    if not force and os.path.exists(mdpath):
+        print(f">> Metadata exists: {mdpath}")
+        return
+    
+    wiki_segment = get_wikiseg_path(wikipath, segment)
+    seg_title_to_info = defaultdict(list)
+    seg_title_to_info_wtext = defaultdict(list)
+    for subseg in sorted(os.listdir(wiki_segment)):
+        if "metadata" in subseg:
+            continue
+        subseg_path = f"{wiki_segment}/{subseg}"
+        #print(f">>     Processing {subseg_path}")
+        with open(f"{wiki_segment}/{subseg}") as f:
+            for i, jl in enumerate(f):
+                l = json.loads(jl)
+                try:
+                    ltitle = l['title']
+                    ldata = {
+                        'id': l['id'],
+                        'has_text': l['text'] != '',
+                        'url': l['url'],
+                    }
+                    seg_title_to_info[ltitle].append(ldata)
+                    if ldata['has_text']:
+                        seg_title_to_info_wtext[ltitle].append({k: v for k, v in ldata.items() if k != 'has_text'})
+                except:
+                    print("Exception!!!")
+                    print(l)
+    
+    # Validate results
+    duplicate_titles = {k: v for k, v in seg_title_to_info.items() if len(v) > 1}
+    assert len(duplicate_titles) == 0, f"Number duplicate titles: {len(duplicate_titles)} for wiki segment: {wiki_segment}"
+    
+    # Write metadata
+    metadata = {
+        "all_titles": dict(seg_title_to_info),
+        "titles_with_text": dict(seg_title_to_info_wtext),
+    }
+    with open(mdpath, 'w+') as mdf:
+        json.dump(metadata, mdf)
+    print(f">> Wrote metadata for {len(seg_title_to_info):6} titles ({len(seg_title_to_info_wtext):6} with text) to {mdpath}")
